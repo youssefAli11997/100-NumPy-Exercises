2043cbe6fb197bb4eddd6f3225a0c9b4d649b9d4
Youssef Ali <youssefali6212@gmail.com>
2017-09-06 22:43:54 +0200

    Add files via upload

---
+++ 100-numpy-challenge.py
@@ -0,0 +1,27 @@
+#01
+import numpy as np
+
+#02
+print(np.__version__)
+np.show_config()
+
+#03
+z = np.zeros(10)
+print(z)
+
+#04
+#python3 -c "import numpy; numpy.info(numpy.add)"
+
+#05
+x = np.zeros(10)
+x[4] = 1
+print(x)
+
+#06
+vec = np.array(range(10:50))
+#or
+vec = np.arange(10,50)
+print(vec)
+
+#07
+
