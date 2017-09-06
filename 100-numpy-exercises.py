91a697777dca90e715a5ec39406afe3e744bffb4
Youssef Ali <youssefali6212@gmail.com>
2017-09-07 02:19:34 +0300

    Update 100-numpy-exercises.py

--- 100-numpy-exercises.py
+++ 100-numpy-exercises.py
@@ -10,7 +10,7 @@ z = np.zeros(10)
 print(z)
 
 #04
-python3 -c "import numpy; numpy.info(numpy.add)"
+#python3 -c "import numpy; numpy.info(numpy.add)"
 
 #05
 x = np.zeros(10)
@@ -20,7 +20,7 @@ print(x)
 #06
 vec = np.array(range(10,50))
 print(vec)
-#or # the same
+#or #the same result
 vec = np.arange(10,50)
 print(vec)
 
@@ -29,3 +29,46 @@ vec = np.arange(1,6)
 vec2 = vec[::-1]
 print(vec)
 print(vec2)
+
+#08
+rng = range(0,9)
+arr = np.array([rng[:3],rng[3:6],rng[6:]])
+print(arr)
+#or #the same result
+arr = np.arange(9).reshape(3,3)
+print(arr)
+
+#09
+vec = np.nonzero([1,2,0,0,4,0])
+print(vec)
+
+#10
+vec = np.eye(3)
+print(vec)
+
+#11
+arr = np.random.random((3,3,3))
+print(arr)
+
+#12
+arr = np.random.random((10,10))
+print(arr, arr.min(), arr.max())
+
+#13
+vec = np.floor(np.random.random((30)) * 50)
+print(vec)
+print(np.mean(vec))
+
+#14
+vec = np.ones((5,5))
+vec[1:-1, 1:-1] = 0
+print(vec)
+
+#15
+"""
+nan
+false
+false
+nan
+false
+"""
