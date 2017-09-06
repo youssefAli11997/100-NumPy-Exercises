b8a2304bec8ca0e624972d9327eb27cd94992092
Youssef Ali <youssefali6212@gmail.com>
2017-09-06 23:49:48 +0300

    Update 100-numpy-exercises.py

--- 100-numpy-exercises.py
+++ 100-numpy-exercises.py
@@ -10,7 +10,7 @@ z = np.zeros(10)
 print(z)
 
 #04
-python3 -c "import numpy; numpy.info(numpy.add)"
+#python3 -c "import numpy; numpy.info(numpy.add)"
 
 #05
 x = np.zeros(10)
@@ -18,10 +18,14 @@ x[4] = 1
 print(x)
 
 #06
-vec = np.array(range(10:50))
-#or
+vec = np.array(range(10,50))
+print(vec)
+#or # the same
 vec = np.arange(10,50)
 print(vec)
 
 #07
-
+vec = np.arange(1,6)
+vec2 = vec[::-1]
+print(vec)
+print(vec2)
